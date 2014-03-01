bf16af1ad24107818ff6ff19f20c6990a5e252e6
ytaddy <ytaddy09@gmail.com>
2014-03-02 02:17:24 +0900

    modify login

--- app/Controller/LoginController.php
+++ app/Controller/LoginController.php
@@ -3,11 +3,11 @@ class LoginController extends AppController {
 
 	public $uses = array('Player');
 
-	public function index () {
+	public function index() {
 		$player_id = $this->request->params['player_id'];
 		$password = $this->reques->params['password'];
 		$res = $this->Player->checkPass($player_id, $password);
 
-		$this->set('hoge', $res);
+		$this->set('player_id', $player_id);
 	}
 }
