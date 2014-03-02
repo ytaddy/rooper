0746c67d8e4132b5287b76f56dd5479ce50fb3f5
ytaddy <ytaddy09@gmail.com>
2014-03-02 21:48:12 +0900

    test

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -31,6 +31,8 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
+	public $uses = array('Player');
+
 	public function beforeFilter() {
 		if (isset($this->request->data['player_id'])
 			&& isset($this->request->data['password'])) {
