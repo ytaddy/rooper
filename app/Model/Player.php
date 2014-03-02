b62c5fc91e9d80acb76611c591875c058fd8fc31
ytaddy <ytaddy09@gmail.com>
2014-03-02 15:28:03 +0900

    test

--- app/Model/Player.php
+++ app/Model/Player.php
@@ -5,6 +5,7 @@ class Player extends AppModel {
 
 	public function checkPass($player_id, $in_password) {
 		$player_data = $this->find('first', array('conditions' => array('Player.player_id' => $player_id)));
+		var_dump($player_data);
 		return ($in_password === $player_data['password']) ? true : false;
 	}
 }
