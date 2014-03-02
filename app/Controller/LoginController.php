57bc1b381f374c1a0929510a836d80e3dc68e96e
ytaddy <ytaddy09@gmail.com>
2014-03-02 14:57:34 +0900

    test

--- app/Controller/LoginController.php
+++ app/Controller/LoginController.php
@@ -4,6 +4,7 @@ class LoginController extends AppController {
 	public $uses = array('Player');
 
 	public function index() {
+		var_dump($this->request);
 		$player_id = $this->request->params['player_id'];
 		$password = $this->reques->params['password'];
 		$res = $this->Player->checkPass($player_id, $password);
